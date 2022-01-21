1631d8f87ae226b9389444315e798a7013a74417
Aleksey <Elchin>
2022-01-21 15:35:39 +0900

    python notebook

--- SkillBox/lesson_009/python_snippets/probe.py
+++ SkillBox/lesson_009/python_snippets/probe.py
@@ -1,2 +1,12 @@
-ba = bytearray(b'hello ')
-print(ba)
+# еще вариант
+file_name = 'pushkin.txt'
+file = open(file_name, mode='r', encoding='utf8')
+line = True
+while line:
+    line = file.readline()
+    if 'красавица' in line:
+        print('Красавица найдена в строке', line)
+        break
+else:
+    print('Тут красавиц нет')
+file.close()

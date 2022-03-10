244a111a91f01fbed5472b204660f152becabd45
Aleksey <Elchin>
2022-03-11 02:53:18 +0900

    django notebook

--- [Илья Фофанов]  Django 3 full stack/password_generator/generator/views.py
+++ [Илья Фофанов]  Django 3 full stack/password_generator/generator/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
+
 
 # Create your views here.
 
 def home(request):
-  return HttpResponse('hello world')
+    the_password = 'testing'
+    return render(request, 'generator/home.html', {password: the_password})
 
-def eggs(request):
-     return HttpResponse('eggs')
+
+def password(request):
+    the_password = ''
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    length = 10
+
+    for x in range(length):
+        the_password += random.choice(characters)
+
+    return render(request, 'generator/password.html', {'password': the_password})
--- [Илья Фофанов]  Django 3 full stack/password_generator/password_generator/urls.py
+++ [Илья Фофанов]  Django 3 full stack/password_generator/password_generator/urls.py
@@ -19,5 +19,5 @@ from generator import views
 
 urlpatterns = [
     path('', views.home),
-    path('eggs', views.eggs),
+    path('password', views.password, name='password'),
 ]

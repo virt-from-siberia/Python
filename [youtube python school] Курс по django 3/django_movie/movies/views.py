a4e141e2eebbd50cc50780db51e9dd44567f3ba7
Aleksey <Elchin>
2022-05-14 17:49:47 +0900

    python pythonscool django3 notebook

--- [youtube python school] Курс по django 3/django_movie/movies/views.py
+++ [youtube python school] Курс по django 3/django_movie/movies/views.py
@@ -10,9 +10,9 @@ from .models import Movie
 class MoviesView(ListView):
     model = Movie
     queryset = Movie.objects.filter(draft=False)
-    template_name = "movies/movies.html"
+#    template_name = "movies/movies.html"
 
 
 class MovieDetailView(DetailView):
     model = Movie
-    slug_field = "url"
+    slug_field = 'url'

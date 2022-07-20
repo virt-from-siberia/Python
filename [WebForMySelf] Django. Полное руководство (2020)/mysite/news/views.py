37af9cf9d1f7dbbc5234e327cb2fabd18c65dd41
aleksey elchin <alonso85@mail.ru>
2022-07-20 23:28:36 +0900

    web for myselth django PC

---
+++ [WebForMySelf] Django. Полное руководство (2020)/mysite/news/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+
+
+def index(request):
+    print(request)
+    return HttpResponse('hello world')
+
+
+def test(request):
+    print(request)
+    return HttpResponse('test page')

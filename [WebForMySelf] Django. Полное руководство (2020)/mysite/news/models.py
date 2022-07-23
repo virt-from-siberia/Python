2e01f4291e7962c46de11e996489bfb5e59eef3f
aleksey elchin <alonso85@mail.ru>
2022-07-23 23:13:57 +0900

    webformyselth PC

--- [WebForMySelf] Django. Полное руководство (2020)/mysite/news/models.py
+++ [WebForMySelf] Django. Полное руководство (2020)/mysite/news/models.py
@@ -12,11 +12,24 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    category = models.ForeignKey('Category', verbose_name='Категория', on_delete=models.PROTECT, null=True)
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
 
     class Meta:
         verbose_name = 'Новость'
         verbose_name_plural = 'Новости'
         ordering = ['-created_at']
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=150, verbose_name='Наименование_категории', db_index=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ['title']

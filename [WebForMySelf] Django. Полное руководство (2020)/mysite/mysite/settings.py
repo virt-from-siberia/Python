ddd4b9aa271cb64630382b76712ebd66aa533157
Aleksey <Elchin>
2022-07-25 03:21:57 +0900

    django webformyselph notebook

--- [WebForMySelf] Django. Полное руководство (2020)/mysite/mysite/settings.py
+++ [WebForMySelf] Django. Полное руководство (2020)/mysite/mysite/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,8 +67,6 @@ TEMPLATES = [
         },
     },
 ]
-
-
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 

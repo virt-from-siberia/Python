f1e20391e2a4fa7c96b8d3d86d0794da1ca4df91
aleksey elchin <alonso85@mail.ru>
2022-08-13 21:38:37 +0900

    webformyselph django PC

--- [WebForMySelf] Django. Полное руководство (2020)/dartblog/suteblog/blog/models.py
+++ [WebForMySelf] Django. Полное руководство (2020)/dartblog/suteblog/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+'''
+    Category
+    ========
+    title, slug
+    
+    Tag
+    ========
+    title, slug
+    
+    Post
+    ========
+    title, slug, content, created_at, photo, 
+'''

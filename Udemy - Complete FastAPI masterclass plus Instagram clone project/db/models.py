f9fd978bb7192054541a1ba916afef01b2d46ff1
aleksey elchin <alonso85@mail.ru>
2022-08-24 16:04:51 +0900

    PC

--- Udemy - Complete FastAPI masterclass plus Instagram clone project/db/models.py
+++ Udemy - Complete FastAPI masterclass plus Instagram clone project/db/models.py
@@ -1,6 +1,9 @@
 from db.database import Base
+
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column
-from sqlalchemy.sql.sqltypes import Integer, String
+from sqlalchemy.sql.schema import ForeignKey
+from sqlalchemy.sql.sqltypes import Integer, String, Boolean
 
 
 class DbUser(Base):
@@ -9,3 +12,13 @@ class DbUser(Base):
     username = Column(String)
     email = Column(String)
     password = Column(String)
+
+
+class DbArticle(Base):
+    __tablename__ = 'articles'
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    content = Column(String)
+    published = Column(Boolean)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship("DbUser", back_populates='')

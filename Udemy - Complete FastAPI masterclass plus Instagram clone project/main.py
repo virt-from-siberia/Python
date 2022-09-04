5e42afe2a222c1ab8583f2e62606d2ce11ad9514
aleksey elchin <alonso85@mail.ru>
2022-09-05 07:15:21 +0900

    Udemy - Complete FastAPI masterclass plus Instagram clone project PC

--- Udemy - Complete FastAPI masterclass plus Instagram clone project/main.py
+++ Udemy - Complete FastAPI masterclass plus Instagram clone project/main.py
@@ -5,6 +5,7 @@ from router import blog_get
 from router import blog_post
 from router import user
 from router import article
+from router import product
 
 from db.database import engine
 from db import models
@@ -17,6 +18,7 @@ from fastapi.responses import JSONResponse, PlainTextResponse
 app = FastAPI()
 app.include_router(user.router)
 app.include_router(article.router)
+app.include_router(product.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
---
+++ Udemy - Complete FastAPI masterclass plus Instagram clone project/router/product.py
@@ -0,0 +1,67 @@
+from typing import Optional, List
+from fastapi import APIRouter, Header
+from fastapi.responses import Response, HTMLResponse, PlainTextResponse
+
+router = APIRouter(
+    prefix='/product',
+    tags=['product']
+)
+
+products = ['watch', 'camera', 'phone']
+
+
+@router.get('/all')
+def get_all_products():
+    # return product
+    data = " ".join(products)
+    return Response(content=data, media_type="text/plain")
+
+
+@router.get('/withheader')
+def get_products(response: Response, custom_header: Optional[List[str]] = Header(None)):
+    response.headers['custom_response_header'] = ", ".join(custom_header)
+    return products
+
+
+@router.get('/{id}', responses={
+    200: {
+        "content": {
+            "text/html": {
+                "example": "<div> Product </div>"
+            }
+        },
+        "description": "Returns the HTML for an object"
+    },
+    404: {
+        "content": {
+            "text/plain": {
+                "example": "Product not awalible"
+            }
+        },
+        "description": "Error message"
+    }
+})
+def get_product(id: int):
+    if id > len(products):
+        out = 'Product not awalible'
+        return PlainTextResponse(status_code=404, content=out, media_type='text/plain')
+    else:
+        product = products[id]
+        out = f"""
+        <head>
+           <style>
+                .product {{
+                    width : 500px;
+                    height : 30px;
+                    border : 2px inset green;
+                    background : lightblue;
+                    text-align : center;
+                }}
+           </style>
+        </head>
+        <div class="product">
+            {product}
+        </div>
+    """
+
+    return HTMLResponse(content=out, media_type='text/html')

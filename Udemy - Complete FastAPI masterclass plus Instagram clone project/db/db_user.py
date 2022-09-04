660422f93447f37334960d12436efd0cbff241d2
aleksey elchin <alonso85@mail.ru>
2022-09-04 13:57:42 +0900

    Complete FastAPI masterclass plus Instagram clone project PC

--- Udemy - Complete FastAPI masterclass plus Instagram clone project/db/db_user.py
+++ Udemy - Complete FastAPI masterclass plus Instagram clone project/db/db_user.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from db.hash import Hash
 from sqlalchemy.orm.session import Session
 from schemas import UserBase
@@ -5,34 +6,46 @@ from db.models import DbUser
 
 
 def create_user(db: Session, request: UserBase):
-  new_user = DbUser(
-    username = request.username,
-    email = request.email,
-    password = Hash.bcrypt(request.password)
-  )
-  db.add(new_user)
-  db.commit()
-  db.refresh(new_user)
-  return new_user
+    new_user = DbUser(
+        username=request.username,
+        email=request.email,
+        password=Hash.bcrypt(request.password)
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
 
 def get_all_users(db: Session):
-  return db.query(DbUser).all()
+    return db.query(DbUser).all()
+
 
 def get_user(db: Session, id: int):
-  return db.query(DbUser).filter(DbUser.id == id).first()
+    user = db.query(DbUser).filter(DbUser.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {id} not found')
+    return db.query(DbUser).filter(DbUser.id == id).first()
+
 
 def update_user(db: Session, id: int, request: UserBase):
-  user = db.query(DbUser).filter(DbUser.id == id)
-  user.update({
-    DbUser.username: request.username,
-    DbUser.email: request.email,
-    DbUser.password: Hash.bcrypt(request.password)
-  })
-  db.commit()
-  return 'ok'
+    user = db.query(DbUser).filter(DbUser.id == id)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {id} not found')
+    user.update({
+        DbUser.username: request.username,
+        DbUser.email: request.email,
+        DbUser.password: Hash.bcrypt(request.password)
+    })
+    db.commit()
+    return 'ok'
+
 
 def delete_user(db: Session, id: int):
-  user = db.query(DbUser).filter(DbUser.id == id).first()
-  db.delete(user)
-  db.commit()
-  return 'ok'
+    user = db.query(DbUser).filter(DbUser.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User with id {id} not found')
+    db.delete(user)
+    db.commit()
+    return 'ok'
+# raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Article with id {id} not found')
--- Udemy - Complete FastAPI masterclass plus Instagram clone project/main.py
+++ Udemy - Complete FastAPI masterclass plus Instagram clone project/main.py
@@ -1,12 +1,18 @@
 from typing import Optional
-from fastapi import FastAPI
+from exceptions import StoryException
+
 from router import blog_get
 from router import blog_post
 from router import user
 from router import article
+
 from db.database import engine
 from db import models
 
+from fastapi import FastAPI
+from fastapi import Request
+from fastapi import HTTPException, status
+from fastapi.responses import JSONResponse, PlainTextResponse
 
 app = FastAPI()
 app.include_router(user.router)
@@ -14,8 +20,23 @@ app.include_router(article.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
+
 @app.get('/hello')
 def index():
-  return {'message': 'Hello world!'}
+    return {'message': 'Hello world!'}
+
+
+@app.exception_handler(StoryException)
+def story_exception_handler(request: Request, exc: StoryException):
+    return JSONResponse(
+        status_code=418,
+        content={'detail': exc.name}
+    )
+
+
+# @app.exception_handler(HTTPException)
+# def custom_handler(request: Request, exc: StoryException):
+#     return PlainTextResponse(str(exc), status_code=400)
+
 
 models.Base.metadata.create_all(engine)

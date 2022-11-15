260d8afa4d7c96ad0db7e1e4b7f65e2c2db319ee
Aleskey Elchin <virtyoz777@MacBook-Pro-Aleksej.local>
2022-11-15 18:13:22 +0900

    fast api course

--- [youtube] Fast API Crash Course Code-along | Build an app with Postgres, SQL Alchemy, Async, and more/main.py
+++ [youtube] Fast API Crash Course Code-along | Build an app with Postgres, SQL Alchemy, Async, and more/main.py
@@ -2,37 +2,13 @@ from typing import Optional, List
 from fastapi import FastAPI, Path, Query
 from pydantic import BaseModel
 
-app = FastAPI()
+from api import user, sections, courses
 
+app = FastAPI(
+    title='My app',
+    description="",
+)
 
-users = []
-
-
-class User(BaseModel):
-    email: str
-    id: int
-    is_active: bool
-    bio: Optional[str]
-
-
-@app.post("/users")
-async def crete_user(user: User):
-    users.append(user)
-    return {"user": user}
-
-
-@app.get("/users", response_model=List[User])
-async def get_user():
-    return users
-
-
-@app.get('/users/{id}')
-async def get_user_by_id(
-        id: int = Path(
-            ..., description='The id of youser you want ot retrive', gt=1,),
-        q: str = Query(None, max_length=5)
-):
-    return {
-        "user":  users[id],
-        "query": q
-    }
+app.include_router(user.router)
+app.include_router(sections.router)
+app.include_router(courses.router)

2a8929cf6586a8430fb2f852e4d4e714f2f062ab
Aleskey Elchin <virtyoz777@MacBook-Pro-Aleksej.local>
2022-12-14 05:43:05 +0900

    fast api youtube

--- [youtube] REST API with FastAPI, SQLAlchemy and PostgreSQL/models.py
+++ [youtube] REST API with FastAPI, SQLAlchemy and PostgreSQL/models.py
@@ -1,7 +1,11 @@
 from database import Base
-from sqlalchemy import String, Boolean, Integer, Column
+from sqlalchemy import String, Boolean, Integer, Column, Text
 
 
 class Item(Base):
     __tablename__ = 'items'
+
     id = Column(Integer, primary_key=True)
+    name = Column(String(255), nullable=False, unique=True)
+    description = Column(Text)
+    on_offer = Column(Boolean, default=False)

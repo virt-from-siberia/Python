ffbde05b4a6fbaf761f0830391f34685fc85f52f
aleksey elchin <alonso85@mail.ru>
2022-12-17 16:47:20 +0900

    fast api

--- [youtube] REST API with FastAPI, SQLAlchemy and PostgreSQL/main.py
+++ [youtube] REST API with FastAPI, SQLAlchemy and PostgreSQL/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 class Item(BaseModel):
     id: int
-    name : str
+    name: str
     price: str
     description: str
     on_offer: bool
@@ -21,48 +21,74 @@ class Item(BaseModel):
 db = SessionLocal()
 
 
-@app.get('/items', response_model=List[Item], status_code=200)
+@app.get('/items', response_model=List[Item], status_code=status.HTTP_200_OK)
 def get_all_items():
-    items = db.query(models.Item).all()    
+    items = db.query(models.Item).all()
     return items
 
 
-@app.get('/item/{item_id} ')
-def get_item(item_id: int):
-    pass
+@app.put('/items/{item_id}', response_model=Item, status_code=status.HTTP_200_OK)
+def update_item(item_id: int, item: Item):
+
+    item = db.query(models.Item).filter(models.Item.id == item_id).first()
+
+    if item is None:
+        raise HTTPException(status_code=400, detail="item do not exist")
+
+    item_to_update = db.query(models.Item).filter(
+        models.Item.id == item_id).first()
+
+    item_to_update.name = item.name
+    item_to_update.price = item.price
+    item_to_update.description = item.description
+    item_to_update.on_offer = item.on_offer
+
+    db.commit()
+    return item_to_update
 
 
 @app.post('/item', response_model=Item, status_code=status.HTTP_201_CREATED)
-def create_item(item : Item):
-    print("item", item)
+def create_item(item: Item):
     new_item = models.Item(
         name=item.name,
         price=item.price,
         description=item.description,
         on_offer=item.on_offer,
     )
-       
-    
+
     db_item = db.query(models.Item).filter(item.name == new_item.name).first()
-    print(db_item)
-    
+
     if db_item is not None:
         raise HTTPException(status_code=400, detail="item already exists")
-    
+
     db.add(new_item)
-    db.commit() 
-    
+    db.commit()
+
     return new_item
 
 
-@app.get('/item/{item_id}')
-def update_item(item_id: int):
-    pass
+@app.get('/items/{item_id}', response_model=Item, status_code=status.HTTP_200_OK)
+def get_item(item_id: int):
+    item = db.query(models.Item).filter(models.Item.id == item_id).first()
+
+    if item is None:
+        raise HTTPException(status_code=400, detail="item do not exist")
+
+    return item
 
 
 @app.delete('/item/{item_id}')
 def delete_item(item_id: int):
-    pass
+    item_to_delete = db.query(models.Item).filter(
+        models.Item.id == item_id).first()
+    
+    if item_to_delete is None:
+        raise HTTPException(status_code=400, detail="item do not exist")
+    
+    db.delete(item_to_delete)
+    db.commit()
+    
+    
 
 
 # @app.get('/')

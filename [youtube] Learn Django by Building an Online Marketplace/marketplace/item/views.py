f3ea7a95b4f4d5663b2f526fecf28e9537792433
Aleksey Elchin <alonso85@mail.ru>
2023-02-09 23:14:42 +0900

    django marketplace

--- [youtube] Learn Django by Building an Online Marketplace/marketplace/item/views.py
+++ [youtube] Learn Django by Building an Online Marketplace/marketplace/item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Item
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 
 
 def detail(request, pk):
@@ -42,3 +42,24 @@ def delete(request, pk):
     item.delete()
 
     return redirect('dashboard:index')
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'title': 'Edit Item',
+        'form': form
+    })

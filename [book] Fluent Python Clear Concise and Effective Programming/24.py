62134e4e6899d6f9ebd70d94658279c015d0f825
Aleksey Elchin <alonso85@mail.ru>
2023-02-11 18:20:07 +0900

    python book

---
+++ [book] Fluent Python Clear Concise and Effective Programming/24.py
@@ -0,0 +1,34 @@
+import collections
+
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit)
+                       for suit in self.suits for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+beer_card = Card('7', 'diamonds')
+
+print('beer_card', beer_card)
+
+Card(rank='7', suit='diamonds')
+
+deck = FrenchDeck()
+print('len(deck)', len(deck))
+
+
+print('deck[0]', deck[0])
+
+

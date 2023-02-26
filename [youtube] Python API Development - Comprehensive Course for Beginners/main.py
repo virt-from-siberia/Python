4498bcd39dd3092ddd98ed314ed0db1424ec9c17
Aleksey Elchin <alonso85@mail.ru>
2023-02-27 04:34:08 +0900

    fast api

--- [youtube] Python API Development - Comprehensive Course for Beginners/main.py
+++ [youtube] Python API Development - Comprehensive Course for Beginners/main.py
@@ -7,7 +7,6 @@ from random import randrange
 
 app = FastAPI()
 
-
 my_posts = [
     {
         "id": 1,
@@ -39,13 +38,23 @@ def _file_post(id: int):
             return post
 
 
+def _find_index_post(id: int):
+    for i, p in enumerate(my_posts):
+        print('i', i)
+        print('p', p)
+        if p['id'] == id:
+            return i
+
+
 @app.get("/")
 async def root():
+    
     return {"message": "Hello World"}
 
 
 @app.get('/posts')
 async def get_posts():
+    
     return {"data": my_posts}
 
 
@@ -54,7 +63,7 @@ async def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 10000000000)
     my_posts.append(post_dict)
-
+    
     return {"post": post_dict}
 
 
@@ -63,7 +72,31 @@ def get_post(id: int, response: Response):
 
     post = _file_post(id)
     if not post:
-        raise HTTPException(status=status.HTTP_404_NOT_FOUND,
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Post not found")
-
     return {"post_detail": post}
+
+
+@app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, response: Response):
+    post = _file_post(id)
+    index = _find_index_post(id)
+    if not index or not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Post not found")
+    my_posts.pop(index)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put('/posts/{id}')
+def update_post(id: int, post: Post):
+    index = _find_index_post(id)
+    if not index:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="Post not found")
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+
+    return {"post": post_dict}

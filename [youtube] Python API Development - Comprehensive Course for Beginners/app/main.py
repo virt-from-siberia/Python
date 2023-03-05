df546f70de2c91c3b30363f8946bcb27c07cd798
Aleksey Elchin <alonso85@mail.ru>
2023-03-05 16:44:50 +0900

    python

--- [youtube] Python API Development - Comprehensive Course for Beginners/app/main.py
+++ [youtube] Python API Development - Comprehensive Course for Beginners/app/main.py
@@ -7,7 +7,7 @@ from psycopg2.extras import RealDictCursor
 from sqlalchemy.orm import Session
 
 
-from . import models
+from . import models, schemas
 from .database import engine, get_db
 
 import time
@@ -51,26 +51,11 @@ my_posts = [
 ]
 
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = True
-
-
 @app.get("/")
 async def root():
 
     return {"message": "Hello World"}
 
-
-@app.get('/sqlalchemy')
-def test_posts(db: Session = Depends(get_db)):
-
-    posts = db.query(models.Post).all()
-
-    return {
-        "data": posts,
-    }
 
 
 @app.get('/posts')
@@ -84,7 +69,7 @@ async def get_posts(db: Session = Depends(get_db)):
 
 
 @app.post('/posts')
-async def create_posts(post: Post, db: Session = Depends(get_db)):
+async def create_posts(post: schemas.CreatePost, db: Session = Depends(get_db)):
     # cursor.execute(
     #     """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
     #     (post.title, post.content, post.published))
@@ -115,30 +100,44 @@ def get_post(id: int,  db: Session = Depends(get_db)):
     return {"post_detail": post}
 
 
-@ app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id: int, response: Response):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (id,))
-    deleted_post = cursor.fetchone()
-    conn.commit()
+@app.delete('/posts/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int,  db: Session = Depends(get_db)):
+    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (id,))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
 
-    if not deleted_post:
+    deleted_post = db.query(models.Post).filter(models.Post.id == id)
+
+    # db.refresh(deleted_post)
+
+    if not deleted_post.first():
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Post not found")
+
+    deleted_post.delete(synchronize_session=False)
+    db.commit()
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@ app.put('/posts/{id}')
-def update_post(id: int, post: Post):
-    cursor.execute(
-        """UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,
-        (post.title, post.content, post.published, id))
+@app.put('/posts/{id}')
+def update_post(id: int, updated_post: schemas.CreatePost, db: Session = Depends(get_db)):
+    # cursor.execute(
+    #     """UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,
+    #     (post.title, post.content, post.published, id))
 
-    updated_posts = cursor.fetchone()
-    conn.commit()
+    # updated_posts = cursor.fetchone()
+    # conn.commit()
 
-    if not updated_posts:
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+
+    if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail="Post not found")
 
-    return {"post": updated_posts}
+    post_query.update(updated_post.dict(), synchronize_session=False)
+    db.commit()
+    # db.refresh(post_query.first())
+
+    return {"post": post_query.first()}
---
+++ [youtube] Python API Development - Comprehensive Course for Beginners/app/schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+
+class CreatePost(PostBase):
+    pass
+
+
+class UpdatePost(PostBase):
+    pass
